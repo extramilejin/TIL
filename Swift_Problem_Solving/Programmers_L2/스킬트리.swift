1ed046ea2566223375534d0a0a5a7a823ef211f3
extramilejin <73225306+extramilejin@users.noreply.github.com>
2021-01-25 17:34:53 +0900

    2021.01.25

--- Swift_Problem_Solving/Programmers_L2/스킬트리.swift
+++ Swift_Problem_Solving/Programmers_L2/스킬트리.swift
@@ -24,7 +24,7 @@ func solution(_ skill:String, _ skill_trees:[String]) -> Int {
         var possibleSkillTree = true
         var asciiSkillTree = skill_tree.utf16.map{$0}
         var skillTreeSize = skill_tree.count
-        var numOfincommingEdges = IncommingEdges
+        var numOfincommingEdges = incommingEdges
         
         for i in 0..<skillTreeSize {
             var indexOfPrecedingSkill = Int(asciiSkillTree[i]) - asciiOfA
