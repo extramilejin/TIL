d9e836ca094024d72323abe8bd37b955008d4420
extramilejin <73225306+extramilejin@users.noreply.github.com>
2021-01-25 17:28:38 +0900

    2021.01.25

---
+++ Swift_Problem_Solving/Programmers_L2/스킬트리.swift
@@ -0,0 +1,43 @@
+// 2021.01.25 extramilejin
+
+// Topological sorted check
+
+import Foundation
+
+func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+    var answer = 0
+    let asciiOfA = Int("A".utf16["A".startIndex])
+    var graph = Array(repeating: [Int](), count: 26)
+    var IncommingEdges = Array (repeating: 0, count: 26)
+    var asciiSkill = skill.utf16.map{$0}
+    
+    for i in 0..<skill.count {
+        var indexOfCurSkill = Int(asciiSkill[i]) - asciiOfA
+        for j in 0..<i {
+            var indexOfPrecedingSkill = Int(asciiSkill[j]) - asciiOfA
+            graph[indexOfPrecedingSkill].append(indexOfCurSkill)
+            IncommingEdges[indexOfCurSkill] += 1
+        }
+    }
+    
+    for skill_tree in skill_trees {
+        var possibleSkillTree = true
+        var asciiSkillTree = skill_tree.utf16.map{$0}
+        var skillTreeSize = skill_tree.count
+        var numOfIncommingEdges = IncommingEdges
+        
+        for i in 0..<skillTreeSize {
+            var indexOfPrecedingSkill = Int(asciiSkillTree[i]) - asciiOfA
+            if numOfIncommingEdges[indexOfPrecedingSkill] != 0 {
+                possibleSkillTree = false
+                break
+            }
+            for j in 0..<graph[indexOfPrecedingSkill].count {
+                var indexOfNextSkill = graph[indexOfPrecedingSkill][j]
+                numOfIncommingEdges[indexOfNextSkill] -= 1
+            }
+        }
+        if possibleSkillTree {answer += 1}
+    }
+    return answer
+}
